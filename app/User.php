07d4db60b3797e2de767ce5e227bfa031dfe9c8a
Ｍindy <mindy8503020312@gmail.com>
2017-03-31 13:27:14 +0800

    修改資料庫名稱及新增Role設定

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','google-id','avatar'
+        'name', 'email', 'password','google-id','avatar','role_id'
     ];
 
     /**
