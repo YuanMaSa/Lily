d73c1e020c0141458051e870be0d861ec9330681
Rita <baby1204w@gmail.com>
2017-04-05 00:50:23 +0800

    建photodetail、process資料表

---
+++ database/migrations/2017_04_04_054409_create_photodetails_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotodetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photodetails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('water');
+            $table->double('take_time');
+            $table->double('L_value');
+            $table->double('a_value');
+            $table->double('b_value');
+            $table->string('photo_url');
+            $table->timestamps();
+            $table->integer('process_id')->unsigned();
+            $table->integer('address_id')->unsigned();
+            $table->foreign('process_id')->references('id')->on('processes')->onDelete('cascade');
+            $table->foreign('address_id')->references('id')->on('addresses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photodetails');
+    }
+}
