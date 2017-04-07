db6ac4749935211387e6b4d767877084971c4dbe
Ｍindy <mindy8503020312@gmail.com>
2017-04-08 01:40:43 +0800

    新增預設填入資料role,process

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('roles')->insert([
+            'name' => '會員',
+        ]);
+        DB::table('processes')->insert([
+            'method' => '低溫製程',
+        ]);
+        DB::table('processes')->insert([
+            'method' => '日曬製程',
+        ]);
+        DB::table('processes')->insert([
+            'method' => '熱風製程',
+        ]);
+        DB::table('processes')->insert([
+            'method' => '其他製程',
+        ]);
     }
 }
