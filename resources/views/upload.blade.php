22aa3307e7eb142817fa3e73df06176ded10eb80
Ｍindy <mindy8503020312@gmail.com>
2017-04-09 18:35:42 +0800

    test upload

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -32,6 +32,16 @@ $(function (){
 </script>
 
 <div class="container">
+   @if (count($errors) > 0)
+   <div class="alert alert-danger">
+      <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+     </ul>
+      </div>
+      @endif
 
 
 	  @if ($message = Session::get('success'))
