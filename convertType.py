6f3d8c89b1f2906811f41d1132c778a873512701
Ｍindy <mindy8503020312@gmail.com>
2017-07-19 18:36:32 +0800

    csv轉型態

---
+++ convertType.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import csv
+f = open('/Users/mindy/laravel-project/Lily/storage/public/lilyflower.csv', 'r')
+b = open('/Users/mindy/laravel-project/Lily/storage/public/lilyflower1.csv','w')
+writer = csv.writer(b)
+next(f)
+array=['process_id', 'L_value', 'a_value', 'b_value','h','s','v','take_time','water']
+writer.writerow(array)
+for row in csv.reader(f):
+    #row.replace("\"","")
+    for i in range(len(row)):
+        row[i]=str(row[i])
+    writer.writerow(row)
+f.close()
+b.close()
+print("convertTypey sucess")
